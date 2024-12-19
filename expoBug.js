fa879ae22e040d3f808f88c9fe106831fa73cc4b
BugHunter-X <askzenai@gmail.com>
2024-12-19 17:36:06 +0100

    Add expoBug.js file

---
+++ expoBug.js
@@ -0,0 +1,1 @@
+This error typically occurs when the Expo CLI is unable to correctly identify or access the necessary Android SDK components during the build process.  It might stem from incorrect environment setup, missing SDK tools, or issues with the Android SDK's installation.
